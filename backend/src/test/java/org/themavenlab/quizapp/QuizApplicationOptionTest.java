878be8f6e6b39a34673672247c112dff4857dd44
Parag NUKALWAR <112768770+parag-n@users.noreply.github.com>
2026-05-03 00:56:22 +0530

    closes #3: Question & Option Deserialization test

--- backend/src/test/java/org/themavenlab/quizapp/QuizApplicationOptionTest.java
+++ backend/src/test/java/org/themavenlab/quizapp/QuizApplicationOptionTest.java
@@ -12,23 +12,43 @@ import org.springframework.boot.test.json.JacksonTester;
 @JsonTest
 public class QuizApplicationOptionTest {
 
-  @Autowired
-  private JacksonTester<Option> json;
+	@Autowired
+	private JacksonTester<Option> json;
 
-  @Test
-  void optionSerializationTest() throws IOException {
-    Option option = new Option(1L,
-        "Filters are called before a request hits the DispatcherServlet. They allow for interception-style, chained processing of web requests for security, timeouts, and other purposes.",
-        true);
-    assertThat(json.write(option)).isStrictlyEqualToJson("expectedOption.json");
+	@Test
+	void optionSerializationTest() throws IOException {
+		Option option = new Option(1L,
+				"Filters are called before a request hits the DispatcherServlet. They allow for interception-style, chained processing of web requests for security, timeouts, and other purposes.",
+				true);
+		assertThat(json.write(option)).isStrictlyEqualToJson("expectedOption.json");
 
-    assertThat(json.write(option)).hasJsonPath("@.id");
-    assertThat(json.write(option)).extractingJsonPathNumberValue("@.id").isEqualTo(1);
-    assertThat(json.write(option)).hasJsonPathStringValue("@.text");
-    assertThat(json.write(option)).extractingJsonPathStringValue("@.text").isEqualTo(
-        "Filters are called before a request hits the DispatcherServlet. They allow for interception-style, chained processing of web requests for security, timeouts, and other purposes.");
-    assertThat(json.write(option)).hasJsonPathBooleanValue("@.isCorrect");
-    assertThat(json.write(option)).extractingJsonPathBooleanValue("@.isCorrect").isEqualTo(true);
-  }
+		assertThat(json.write(option)).hasJsonPath("@.id");
+		assertThat(json.write(option)).extractingJsonPathNumberValue("@.id").isEqualTo(1);
+		assertThat(json.write(option)).hasJsonPathStringValue("@.text");
+		assertThat(json.write(option)).extractingJsonPathStringValue("@.text").isEqualTo(
+				"Filters are called before a request hits the DispatcherServlet. They allow for interception-style, chained processing of web requests for security, timeouts, and other purposes.");
+		assertThat(json.write(option)).hasJsonPathBooleanValue("@.isCorrect");
+		assertThat(json.write(option)).extractingJsonPathBooleanValue("@.isCorrect").isEqualTo(true);
+	}
+
+	@Test
+	void optionDeserializationTest() throws IOException {
+		String expected = """
+				{
+				  "id": 1,
+				  "text": "Filters are called before a request hits the DispatcherServlet. They allow for interception-style, chained processing of web requests for security, timeouts, and other purposes.",
+					"isCorrect": true
+				}
+				""";
+
+		assertThat(json.parse(expected)).isEqualTo(new Option(1L,
+				"Filters are called before a request hits the DispatcherServlet. They allow for interception-style, chained processing of web requests for security, timeouts, and other purposes.",
+				true));
+
+		assertThat(json.parseObject(expected).id()).isEqualTo(1L);
+		assertThat(json.parseObject(expected).text()).isEqualTo(
+				"Filters are called before a request hits the DispatcherServlet. They allow for interception-style, chained processing of web requests for security, timeouts, and other purposes.");
+		assertThat(json.parseObject(expected).isCorrect()).isEqualTo(true);
+	}
 
 }
--- backend/src/test/java/org/themavenlab/quizapp/QuizApplicationQuestionTest.java
+++ backend/src/test/java/org/themavenlab/quizapp/QuizApplicationQuestionTest.java
@@ -12,21 +12,39 @@ import org.springframework.boot.test.json.JacksonTester;
 @JsonTest
 public class QuizApplicationQuestionTest {
 
-    @Autowired
-    private JacksonTester<Question> json;
+	@Autowired
+	private JacksonTester<Question> json;
 
-    @Test
-    void questionSerializationTest() throws IOException {
-        Question question = new Question(1L, "How filters are used in Spring Web?", "Spring Framework");
-        assertThat(json.write(question)).isStrictlyEqualToJson("expectedQuestion.json");
+	@Test
+	void questionSerializationTest() throws IOException {
+		Question question = new Question(1L, "How filters are used in Spring Web?", "Spring Framework");
+		assertThat(json.write(question)).isStrictlyEqualToJson("expectedQuestion.json");
 
-        assertThat(json.write(question)).hasJsonPath("@.id");
-        assertThat(json.write(question)).extractingJsonPathNumberValue("@.id").isEqualTo(1);
-        assertThat(json.write(question)).hasJsonPathStringValue("@.text");
-        assertThat(json.write(question)).extractingJsonPathStringValue("@.text").isEqualTo(
-                "How filters are used in Spring Web?");
-        assertThat(json.write(question)).hasJsonPathStringValue("@.category");
-        assertThat(json.write(question)).extractingJsonPathStringValue("@.category").isEqualTo(
-                "Spring Framework");
-    }
+		assertThat(json.write(question)).hasJsonPath("@.id");
+		assertThat(json.write(question)).extractingJsonPathNumberValue("@.id").isEqualTo(1);
+		assertThat(json.write(question)).hasJsonPathStringValue("@.text");
+		assertThat(json.write(question)).extractingJsonPathStringValue("@.text").isEqualTo(
+				"How filters are used in Spring Web?");
+		assertThat(json.write(question)).hasJsonPathStringValue("@.category");
+		assertThat(json.write(question)).extractingJsonPathStringValue("@.category").isEqualTo(
+				"Spring Framework");
+	}
+
+	@Test
+	void questionDeserializationTest() throws IOException {
+		String expected = """
+				{
+				  "id": 1,
+				  "text": "How filters are used in Spring Web?",
+				  "category": "Spring Framework"
+				}
+				""";
+
+		assertThat(json.parse(expected))
+				.isEqualTo(new Question(1L, "How filters are used in Spring Web?", "Spring Framework"));
+
+		assertThat(json.parseObject(expected).id()).isEqualTo(1L);
+		assertThat(json.parseObject(expected).text()).isEqualTo("How filters are used in Spring Web?");
+		assertThat(json.parseObject(expected).category()).isEqualTo("Spring Framework");
+	}
 }
